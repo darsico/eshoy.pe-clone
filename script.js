93c7ddbc6077d6a475eccf292afb2b46a3dcafdb
Diego <diegojairhl@gmail.com>
2022-02-07 19:22:10 -0500

    Efecto de header implementado

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ const alianzas = document.getElementById("menu-alianzas");
 const trabajamos = document.getElementById("menu-trabajamos");
 const contribuimos = document.getElementById("menu-contribuimos");
 const contacto = document.getElementById("menu-contacto");
+const header = document.querySelector(".header");
+
+window.onscroll = function () {
+  const top = window.scrollY;
+  top >= 100
+    ? header.classList.add("active")
+    : header.classList.remove("active");
+};
 
 menuIcon.addEventListener("click", show);
 close.addEventListener("click", hide);
